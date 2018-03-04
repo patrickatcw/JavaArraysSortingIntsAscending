d4edf6ecf23eaf8b0a825fe3b9169525835ccc8a
patrickatcw <pj8392@yahoo.com>
2018-03-04 09:48:06 -0500

    initial structure, directions, updated gitignore file

---
+++ src/com/me/Main.java
@@ -0,0 +1,16 @@
+package com.me;
+//directions
+/*
+- create a program using arrays that sort a list of integers in ascending order
+- set up the program so that the numbers to sort are read in from the keyboard
+- implement the following methods - getIntegers, printArray, and sortIntegers
+- getIntegers returns an array of entered integers from keyboard
+- printArray prints out the contents of the array
+and sortIntegers should sort the array and return a new array containing the sorted numbers
+-you will have to figure out how to copy the array elements from the passed array into a new
+array and sort them and return the new sorted array
+ */
+
+
+public class Main {
+}
