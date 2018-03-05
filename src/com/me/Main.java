813179fcb3cac8dee9f140528bcbab5b0935480b
patrickatcw <pj8392@yahoo.com>
2018-03-05 10:12:16 -0500

    added method for sorting and while loop to go through array and change order

--- src/com/me/Main.java
+++ src/com/me/Main.java
@@ -12,6 +12,7 @@ array and sort them and return the new sorted array
  */
 
 //step 1
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -24,11 +25,14 @@ public class Main {
         //step 2 create int array
         int[] myIntegers = getIntegers(4);
 
+
+
     }
 
     //step 3 method to get ints, from step 2
     public static int[] getIntegers(int capacity) {     //parameter in method name
         int[] array = new int[capacity];                //defining our array, not capacity
+        //the message displayed when initiate program
         System.out.println("Enter " + capacity + " integer values: \r"); //r is for input in console
         for (int i = 0; i < array.length; i++) {        //looping through array
             array[i] = scanner.nextInt();  //for console input
@@ -38,14 +42,45 @@ public class Main {
         return array;
     }
 
-    //step 5 next create print array method
+    //step 5 next create print array method, accept array and printout contents
     //for step 2, step 3, step 4
     public static void printArray(int[] array) {
-        for(int i = 0; i < array.length; i++ ) {        //looping through to print array
+        for (int i = 0; i < array.length; i++) {        //looping through to print array
             //System.out.println("Element " + i + " contents " + array[i]);
         }
 
     }
 
+    //step 6 method for sorting, creates a copy of array passed to a sorted array
+    public static int[] sortIntegers(int[] array) {//passing the unsorted array
+        int[] sortedArray = new int[array.length];  //creating an array that is same size as array that has been passed
+        for (int i = 0; i < array.length; i++) {    //loops through to put values in
+            sortedArray[i] = array[i];              //creates new sorted array
+        }
+
+        //this takes a copy of the array with the right length and automatically makes a new array called
+        //sortedArray for us
+        //step 7 while loop to actually sort the array
+        boolean flag = true;            //set boolean to true
+        int temp;                       //stores num temporarily
+        while (flag) {
+            flag = false;       //continue while loop if only assigned flag to true
+            //checking code
+            //loop will continue only after all have been sorted in order desired
+            for (int i = 0; i < sortedArray.length - 1; i++) {
+                if (sortedArray[i] > sortedArray[i + 1]) {  //swapping ints
+                    temp = sortedArray[i];  //stores current value of sorted array temporarily
+                    sortedArray[i] = sortedArray[i + 1];
+                    sortedArray[i + 1] = temp;
+                    flag = true;   //while loop will continue processing until false
+                    //then exist out
+                }
+            }
+        }
+
+        //step 8 return
+        return sortedArray;
+
+    }
 
 }
